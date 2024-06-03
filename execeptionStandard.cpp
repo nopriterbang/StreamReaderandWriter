78295f40c922a1e69b3fa24aaf3bfb7d047c02de
nopriterbang <novalpriya@gmail.com>
2024-06-03 15:18:43 +0700

    menambahkan fungsi main untuk  penanganan exception untuk akses array

---
+++ execeptionStandard.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <exception>
+// untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan digunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; // penanda 1:...
+    try {
+        array<int, 3> data = {8, 4, 2};
+        // pesan array interger 3 elemen
+        cout <<data.at(5)<< endl;
+    }
+    catch (exception& e) {
+        // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
+}
