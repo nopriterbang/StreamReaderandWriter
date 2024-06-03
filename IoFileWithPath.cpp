c07df9258058ca86f61543577073f362b0abdbdd
nopriterbang <novalpriya@gmail.com>
2024-06-03 14:59:42 +0700

    menambahkan perulangan untuk membuka file dan membuat tampilan jika file tidak ditemukan

--- IoFileWithPath.cpp
+++ IoFileWithPath.cpp
@@ -21,5 +21,36 @@ int main() {
     while (true) {
         cout << "_ ";
         // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika memasukan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl; 
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris 
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+   else cout << "Unable to open file";
+    return 0;
+
 }
