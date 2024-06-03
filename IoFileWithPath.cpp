e8fbf7311a644178596dc67a98109812dda33e14
nopriterbang <novalpriya@gmail.com>
2024-06-03 14:47:12 +0700

    menambahkan fungsi main untuk membuka dan menujuk file serta menulis

--- IoFileWithPath.cpp
+++ IoFileWithPath.cpp
@@ -2,3 +2,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan nama file : ";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "_ ";
+        // mendapatkan setiap karakter dalam satu baris
+    }
+}
