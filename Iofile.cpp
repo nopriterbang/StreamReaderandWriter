1dcef1e59039fb1a179a327c6f324768cbf1d0b8
nopriterbang <novalpriya@gmail.com>
2024-06-03 14:37:33 +0700

    menambahkan perulangan untuk membuka

--- Iofile.cpp
+++ Iofile.cpp
@@ -23,4 +23,29 @@ int main() {
         // menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    // Membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+             // dan tampilan disini
+             cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+
 }
